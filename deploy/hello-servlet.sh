e4a9a2caa3488fc3466f67af296930d33ed5faa0
Marcin Kudla <marcin.kudla@gmail.com>
2016-10-25 14:15:30 +0200

    start script added

---
+++ deploy/hello-servlet.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+APP_NAME="hello-servlet-app"
+
+case "$1" in
+    start)
+        /sbin/start-stop-daemon -S -m -d ./ -p ${APP_NAME}.pid -x /usr/bin/java -- -jar ${APP_NAME}.jar
+        echo "Hello servlet started"
+        ;;
+    restart)
+        /sbin/start-stop-daemon -K -p ${APP_NAME}.pid &>/dev/null
+        /sbin/start-stop-daemon -S -m -d ./ -p ${APP_NAME}.pid -x /usr/bin/java -- -jar ${APP_NAME}.jar
+        echo "Hello servlet restarted"
+        ;;
+    stop)
+        /sbin/start-stop-daemon -K -p ${APP_NAME}.pid &>/dev/null
+        echo "Hello servlet stopped"
+        ;;
+    *)
+        echo "Usage: $0 start|stop application" >&2
+        exit 3
+        ;;
+esac
+
